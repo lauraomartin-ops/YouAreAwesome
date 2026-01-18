2682caf6f721e1d71c4e4bf3ff74dc184df81524
Laura Martin Davis <lauraomartin@icloud.com>
2026-01-18 16:20:29 -0500

    After adding Button

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am a Programmer!"
     var body: some View {
         
         VStack {
             Image(systemName: "convertible.side.fill").resizable()
                 .scaledToFit()
                 .foregroundStyle(.tint)
-            Text("Everything is Awesome!")
+            Text(message)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundStyle(.green)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
         
